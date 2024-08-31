fc6ea62ca9c76216ef2837ef02b01a65374ed1e8
AkshayMohane1819 <akshaymohane.unisoft@gmail.com>
2024-08-31 12:04:39 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict" // Treat all js code as newer version 
+// alert ("hello") we are using node.js not browser, alert is not using on node.js it cant find reuslt
+
+let name = "akshay"        // string value
+let age  = 26              // number or integer value
+let longgedin = false     // boolean value
+
+// string => ""
+// BigInt large number of value
+// number   => 2 to power 53
+// boolean  => true/ false if value correct or not
+// null => standalone value or value is empty
+// undefind => value is not assigned
+// symbol => unique
+
+
+// object =>
+console.table([name,age,longgedin])
+console.log(typeof null);
+
---
+++ 01_basic/logindetails.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "akshay@gmail.com"
+var accountPassword = "123456"
+accountCity ="jaipur"
+
+accountEmail = "aditi@gail.com"
+accountPassword = "15989"
+accountCity = "nagpur"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity]);
