19e3197f99c73a81a81fe0dbd93c8b5f1b3e96aa
AkshayMohane1819 <akshaymohane.unisoft@gmail.com>
2024-09-01 05:07:24 +0000

    Opration config

---
+++ 01_basic/Opration.js
@@ -0,0 +1,15 @@
+
+Postfix 
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+Prefix
+let a = 2;
+let b = ++a;
+console.log(`a:${a},b:${b}`);
+
+// in prefix a value is increment by +1 and the result of both variable are same
+// in postfix a value is increment by +1 so first value of x is same after it increment by 1
+// so postfix value x value is increment by +1 and y is same
